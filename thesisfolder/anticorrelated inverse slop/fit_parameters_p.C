d2f8c75df93da2e0c434f376d43f67fd7756caba
nehcizniy <zichen_yin@yahoo.com>
2024-08-19 12:25:20 -0500

    coding

--- thesisfolder/anticorrelated inverse slop/fit_parameters_p.C
+++ thesisfolder/anticorrelated inverse slop/fit_parameters_p.C
@@ -70,3 +70,197 @@ parameter_steeper[1][3] = {0,0};
 parameter_point[1][4] = {0,0};
 parameter_flat[1][4] = {0,0};
 parameter_steeper[1][4] = {0,0};
+
+//phenix-p-dAu-200
+parameter_point[2][0] = {0,0};
+parameter_flat[2][0] = {0,0};
+parameter_steeper[2][0] = {0,0};
+
+parameter_point[2][1] = {0,0};
+parameter_flat[2][1] = {0,0};
+parameter_steeper[2][1] = {0,0};
+
+parameter_point[2][2] = {0,0};
+parameter_flat[2][2] = {0,0};
+parameter_steeper[2][2] = {0,0};
+
+parameter_point[2][3] = {0,0};
+parameter_flat[2][3] = {0,0};
+parameter_steeper[2][3] = {0,0};
+
+parameter_point[2][4] = {0,0};
+parameter_flat[2][4] = {0,0};
+parameter_steeper[2][4] = {0,0};
+
+//phenix-p-pp-200
+parameter_point[3][0] = {0,0};
+parameter_flat[3][0] = {0,0};
+parameter_steeper[3][0] = {0,0};
+
+//phenix-pbar-AuAu-200-2004
+parameter_point[4][0] = {0,0};
+parameter_flat[4][0] = {0,0};
+parameter_steeper[4][0] = {0,0};
+
+parameter_point[4][1] = {0,0};
+parameter_flat[4][1] = {0,0};
+parameter_steeper[4][1] = {0,0};
+
+parameter_point[4][2] = {0,0};
+parameter_flat[4][2] = {0,0};
+parameter_steeper[4][2] = {0,0};
+
+parameter_point[4][3] = {0,0};
+parameter_flat[4][3] = {0,0};
+parameter_steeper[4][3] = {0,0};
+
+parameter_point[4][4] = {0,0};
+parameter_flat[4][4] = {0,0};
+parameter_steeper[4][4] = {0,0};
+
+parameter_point[4][5] = {0,0};
+parameter_flat[4][5] = {0,0};
+parameter_steeper[4][5] = {0,0};
+
+parameter_point[4][6] = {0,0};
+parameter_flat[4][6] = {0,0};
+parameter_steeper[4][6] = {0,0};
+
+parameter_point[4][7] = {0,0};
+parameter_flat[4][7] = {0,0};
+parameter_steeper[4][7] = {0,0};
+
+parameter_point[4][8] = {0,0};
+parameter_flat[4][8] = {0,0};
+parameter_steeper[4][8] = {0,0};
+
+parameter_point[4][9] = {0,0};
+parameter_flat[4][9] = {0,0};
+parameter_steeper[4][9] = {0,0};
+
+parameter_point[4][10] = {0,0};
+parameter_flat[4][10] = {0,0};
+parameter_steeper[4][10] = {0,0};
+
+//phenix-pbar-AuAu-200
+parameter_point[5][0] = {0,0};
+parameter_flat[5][0] = {0,0};
+parameter_steeper[5][0] = {0,0};
+
+parameter_point[5][1] = {0,0};
+parameter_flat[5][1] = {0,0};
+parameter_steeper[5][1] = {0,0};
+
+parameter_point[5][2] = {0,0};
+parameter_flat[5][2] = {0,0};
+parameter_steeper[5][2] = {0,0};
+
+parameter_point[5][3] = {0,0};
+parameter_flat[5][3] = {0,0};
+parameter_steeper[5][3] = {0,0};
+
+parameter_point[5][4] = {0,0};
+parameter_flat[5][4] = {0,0};
+parameter_steeper[5][4] = {0,0};
+
+//phenix-pbar-dAu-200
+parameter_point[6][0] = {0,0};
+parameter_flat[6][0] = {0,0};
+parameter_steeper[6][0] = {0,0};
+
+parameter_point[6][1] = {0,0};
+parameter_flat[6][1] = {0,0};
+parameter_steeper[6][1] = {0,0};
+
+parameter_point[6][2] = {0,0};
+parameter_flat[6][2] = {0,0};
+parameter_steeper[6][2] = {0,0};
+
+parameter_point[6][3] = {0,0};
+parameter_flat[6][3] = {0,0};
+parameter_steeper[6][3] = {0,0};
+
+parameter_point[6][4] = {0,0};
+parameter_flat[6][4] = {0,0};
+parameter_steeper[6][4] = {0,0};
+
+//phenix-pbar-pAl-200
+parameter_point[7][0] = {0,0};
+parameter_flat[7][0] = {0,0};
+parameter_steeper[7][0] = {0,0};
+
+parameter_point[7][1] = {0,0};
+parameter_flat[7][1] = {0,0};
+parameter_steeper[7][1] = {0,0};
+
+parameter_point[7][2] = {0,0};
+parameter_flat[7][2] = {0,0};
+parameter_steeper[7][2] = {0,0};
+
+parameter_point[7][3] = {0,0};
+parameter_flat[7][3] = {0,0};
+parameter_steeper[7][3] = {0,0};
+
+//phenix-pbar-pAu-200
+parameter_point[8][0] = {0,0};
+parameter_flat[8][0] = {0,0};
+parameter_steeper[8][0] = {0,0};
+
+parameter_point[8][1] = {0,0};
+parameter_flat[8][1] = {0,0};
+parameter_steeper[8][1] = {0,0};
+
+parameter_point[8][2] = {0,0};
+parameter_flat[8][2] = {0,0};
+parameter_steeper[8][2] = {0,0};
+
+parameter_point[8][3] = {0,0};
+parameter_flat[8][3] = {0,0};
+parameter_steeper[8][3] = {0,0};
+
+parameter_point[8][4] = {0,0};
+parameter_flat[8][4] = {0,0};
+parameter_steeper[8][4] = {0,0};
+
+parameter_point[8][5] = {0,0};
+parameter_flat[8][5] = {0,0};
+parameter_steeper[8][5] = {0,0};
+
+//phenix-pbar-pp-200
+parameter_point[9][0] = {0,0};
+parameter_flat[9][0] = {0,0};
+parameter_steeper[9][0] = {0,0};
+
+//phenix-proton-3HeAu-200
+parameter_point[10][0] = {0,0};
+parameter_flat[10][0] = {0,0};
+parameter_steeper[10][0] = {0,0};
+
+parameter_point[10][1] = {0,0};
+parameter_flat[10][1] = {0,0};
+parameter_steeper[10][1] = {0,0};
+
+parameter_point[10][2] = {0,0};
+parameter_flat[10][2] = {0,0};
+parameter_steeper[10][2] = {0,0};
+
+parameter_point[10][3] = {0,0};
+parameter_flat[10][3] = {0,0};
+parameter_steeper[10][3] = {0,0};
+
+parameter_point[10][4] = {0,0};
+parameter_flat[10][4] = {0,0};
+parameter_steeper[10][4] = {0,0};
+
+//phenix-proton-CuAu-200
+parameter_point[11][0] = {0,0};
+parameter_flat[11][0] = {0,0};
+parameter_steeper[11][0] = {0,0};
+
+parameter_point[11][1] = {0,0};
+parameter_flat[11][1] = {0,0};
+parameter_steeper[11][1] = {0,0};
+
+parameter_point[11][2] = {0,0};
+parameter_flat[11][2] = {0,0};
+parameter_steeper[11][2] = {0,0};
